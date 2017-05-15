251d3d3b9f46da756746f54f14a0ca5b916b0e86
Wayne Lewis <wlewis@ospreydcs.com>
2017-05-15 18:43:03 +0300

    Initial commit of blank IOC

---
+++ iocBoot/ioccs_vcm/st.cmd
@@ -0,0 +1,18 @@
+#!../../bin/linux-x86_64/cs_vcm
+
+## You may have to change cs_vcm to something else
+## everywhere it appears in this file
+
+#< envPaths
+
+## Register all support components
+dbLoadDatabase("../../dbd/cs_vcm.dbd",0,0)
+cs_vcm_registerRecordDeviceDriver(pdbbase) 
+
+## Load record instances
+dbLoadRecords("../../db/cs_vcm.db","user=control")
+
+iocInit()
+
+## Start any sequence programs
+#seq snccs_vcm,"user=control"
