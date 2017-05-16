f50ab5fba42e05e3ad035dc08d969d49f5cf7938
Wayne Lewis <wlewis@ospreydcs.com>
2017-05-15 18:58:33 -0700

    Add startup script

--- iocBoot/ioccs_vcm/st.cmd
+++ iocBoot/ioccs_vcm/st.cmd
@@ -3,16 +3,16 @@
 ## You may have to change cs_vcm to something else
 ## everywhere it appears in this file
 
-#< envPaths
+< envPaths
+
+cd ${TOP}
 
 ## Register all support components
-dbLoadDatabase("../../dbd/cs_vcm.dbd",0,0)
+dbLoadDatabase("dbd/cs_vcm.dbd",0,0)
 cs_vcm_registerRecordDeviceDriver(pdbbase) 
 
 ## Load record instances
-dbLoadRecords("../../db/cs_vcm.db","user=control")
+dbLoadRecords("db/3jack_mirror.db","P=D08-OP-,Q=VCM:,M1=STP-Y1,M2=STP-Y2,M3=STP-Y3")
 
 iocInit()
 
-## Start any sequence programs
-#seq snccs_vcm,"user=control"
