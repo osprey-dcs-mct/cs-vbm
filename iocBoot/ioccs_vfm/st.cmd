0690ae79de8803e15a4d70a63982b4a18adc01c0
Wayne Lewis <wlewis@ospreydcs.com>
2017-06-03 00:42:27 +0300

    Add IOC status information

--- iocBoot/ioccs_vfm/st.cmd
+++ iocBoot/ioccs_vfm/st.cmd
@@ -5,6 +5,8 @@
 
 < envPaths
 
+epicsEnvSet("IOC_PREFIX","D08-CS-IOC-MC01")
+
 cd ${TOP}
 
 ## Register all support components
@@ -13,6 +15,8 @@ cs_vfm_registerRecordDeviceDriver(pdbbase)
 
 ## Load record instances
 dbLoadRecords("db/3jack_mirror.db","P=D08-OP-,Q=VFM:,M1=VFM-STP-Y1,M2=VFM-STP-Y2,M3=VFM-STP-Y3,LENGTH=1500,WIDTH=250")
+dbLoadRecords("db/iocAdminSoft.db", "IOC=$(IOC_PREFIX)")
+dbLoadRecords("db/save_restoreStatus.db", "P=$(IOC_PREFIX)")
 
 iocInit()
 
