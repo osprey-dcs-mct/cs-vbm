0aa8d67e2daeb28a62d8fc27cd0a3103f95c875c
Wayne Lewis <wlewis@ospreydcs.com>
2022-04-12 11:31:50 -0700

    Change upstream motor order

--- iocBoot/ioccs_vbm/st.cmd
+++ iocBoot/ioccs_vbm/st.cmd
@@ -9,9 +9,7 @@ epicsEnvSet("ENGINEER", "Engineer")
 epicsEnvSet("LOCATION", "Location")
 
 epicsEnvSet("AS_PATH", "${TOP}/autosave")
-epicsEnvSet("IOC_PREFIX","D08-CS-IOC-CS04")
-epicsEnvSet("EPICS_CA_ADDR_LIST", "10.3.1.255")
-epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", "NO")
+epicsEnvSet("IOC_PREFIX","MCT:CS_Y_VBM")
 
 cd ${TOP}
 
@@ -20,7 +18,7 @@ dbLoadDatabase("dbd/cs_vbm.dbd",0,0)
 cs_vbm_registerRecordDeviceDriver(pdbbase) 
 
 ## Load record instances
-dbLoadRecords("db/3jack_mirror.db","P=D08-OP-,Q=VFM:,M1=VFM-STP-Y1,M2=VFM-STP-Y2,M3=VFM-STP-Y3,LENGTH=1500,WIDTH=250")
+dbLoadRecords("db/3jack_mirror.db","P=MCT:VBM:,Q=CS_Y:,M1=US_OB,M2=US_IB,M3=DS,LENGTH=1500,WIDTH=250")
 dbLoadRecords("db/iocAdminSoft.db", "IOC=$(IOC_PREFIX)")
 dbLoadRecords("db/save_restoreStatus.db", "P=$(IOC_PREFIX):")
 
